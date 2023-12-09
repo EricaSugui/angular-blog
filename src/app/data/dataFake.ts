93391a25d404764f9ba278bb9d7bcedf7aa1a2de
EricaSugui <ericasugui@gmail.com>
2023-12-09 10:01:14 -0300

    chore: add ngfor for dynamic small-card

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,20 +7,26 @@ export const dataFake = [
   },
   {
     "id": "2",
-    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
-    "title": "Cerejeiras em Sorocaba2",
+    "photo": "https://www.folhadelondrina.com.br/img/inline/2990000/0x800/Cerejeiras-florescem-e-embelezam-ruas-de-Apucarana0299618800202006181742.webp?fallback=https%3A%2F%2Fwww.folhadelondrina.com.br%2Fimg%2Finline%2F2990000%2FCerejeiras-florescem-e-embelezam-ruas-de-Apucarana0299618800202006181742.jpg%3Fxid%3D5228320&xid=5228320",
+    "title": "Florada da cerejeira embeleza Apucarana",
     "description": "texto2 texto2 texto2"
   },
   {
     "id": "3",
-    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
-    "title": "Cerejeiras em Sorocaba3",
+    "photo": "https://veja.abril.com.br/wp-content/uploads/2016/12/cerejeira-cherry-blossom-sakura-20160420-005.jpg?quality=70&strip=all",
+    "title": "Sakura, a cerejeira do Japão",
     "description": "texto3 texto3 texto3"
   },
   {
     "id": "4",
-    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
-    "title": "Cerejeiras em Sorocaba4",
+    "photo": "https://imagens-revista.vivadecora.com.br/uploads/2022/05/A-flor-de-cerejeira-significado-%C3%A9-amor-beleza-e-esperanca-Foto-iStock.jpg",
+    "title": "Flor de cerejeira: Significado, lendas e + 35 ideias de decoração.",
     "description": "texto4 texto4 texto4"
-  }
+  },
+  {
+    "id": "5",
+    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
+    "title": "5 Cerejeiras em Sorocaba",
+    "description": "texto5 texto5 texto5"
+  },
 ]
