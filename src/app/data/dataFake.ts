93ac5f168f77ad253557a9c182b605961531ce3b
EricaSugui <ericasugui@gmail.com>
2023-12-09 00:13:54 -0300

    chore: use mock to content pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
+    "title": "Cerejeiras em Sorocaba",
+    "description": "texto texto texto"
+  },
+  {
+    "id": "2",
+    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
+    "title": "Cerejeiras em Sorocaba2",
+    "description": "texto2 texto2 texto2"
+  },
+  {
+    "id": "3",
+    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
+    "title": "Cerejeiras em Sorocaba3",
+    "description": "texto3 texto3 texto3"
+  },
+  {
+    "id": "4",
+    "photo": "https://s2-g1.glbimg.com/hT4uikZ4aa3qQl4cSrjhIYxsFNg=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/n/JsWJOERby7AuZBbvxMPA/nc-florada-cerejeira-300723.jpg3.jpg",
+    "title": "Cerejeiras em Sorocaba4",
+    "description": "texto4 texto4 texto4"
+  }
+]
